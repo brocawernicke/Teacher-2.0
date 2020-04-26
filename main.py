64f30eee05493b1a873be9328bf8305c5d6f7f97
Sangkyuai <sangkyu.ai@gmail.com>
2020-04-26 10:50:57 +0000

    0426 수업

--- main.py
+++ main.py
@@ -1,40 +1,54 @@
-# 파이썬 게임 패키지 로딩
+# 파이썬 게임 패키지 사용
 import pygame
 
-# 편의를 위해 컬러 상수 지정
+# 색깔 지정
 BLACK = (0, 0, 0)
 WHITE = (255, 255, 255)
 RED = (255, 0, 0)
 GREEN = (0, 255, 0)
 BLUE = (0, 0, 255)
 
-# 게임 코딩 시작을 위한 초기화
+#  게임 준비
 pygame.init()
 done = False
 clock = pygame.time.Clock()
 screen = pygame.display.set_mode([800,450])
+buttons = []
+x = 400
+y = 225
 
 # 무한 루프
 while not done:
     clock.tick(10)
 
-    # (pygame event)* 처리
+    # 게임 이벤트 처리
     for event in pygame.event.get():
-        # 종료 이벤트(quit event) 처리
-        if event.type == pygame.QUIT:
+        # 'KEYDOWN' 이벤트 처리
+        if event.type == pygame.KEYDOWN:
+            pressed = pygame.key.get_pressed()
+            buttons = [pygame.key.name(k) for k,v in enumerate(pressed) if v]
+        # 'QUIT' 이벤트 처리
+        elif event.type ==pygame.QUIT:
             done = True
+
+        # 'buttons' 처리
+        for btn in buttons:
+            if btn == 'right':
+                x = x + 10
+            elif btn == 'left':
+                x = x - 10
 
         screen.fill(WHITE)
 
-        # (가상 화면에) 그림을 그리는 명령 입력
-        pygame.draw.circle(screen, RED, [400, 225], 50, 3)
+        # 그림 그리는 명령 입력
         pygame.draw.line(screen, RED, [0, 0], [800, 450], 1)
         pygame.draw.lines(screen, BLUE, False, [[100, 100], [200, 100], [200, 200], [100, 200]], 3)
-        pygame.draw.rect(screen, GREEN, [400, 100, 50, 50], 0)
+        pygame.draw.rect(screen, GREEN, [x, y, 50, 50], 0)
         pygame.draw.polygon(screen, RED, [[400, 400], [500, 400], [450, 300]], 5)
 
-        # PC 화면에 그림을 그리라고 하는 최종 명령
+        # 실제 그림을 그리는 단계
         pygame.display.flip()
 
 # 게임 종료 처리
 pygame.quit()
+
